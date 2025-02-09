b47c4a1f084543f2f22f8b12f9679219ba5c94fb
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-02-09 15:12:27 +0330

    t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { I18nProvider } from "@/providers/i18n-provider";
-import { siteConfig } from "@/config/metadata";
 import { ClerkProvider } from '@clerk/nextjs'
 import HeaderServer from "@/components/Header/HeaderServer";
 import { Provider } from "react-redux";
@@ -24,42 +23,6 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-};
-
-// metadata
-
-export const metadata = {
-  metadataBase: new URL('https://tradingai.com'),
-  title: siteConfig.title,
-  description: siteConfig.description,
-  keywords: siteConfig.keywords,
-  authors: siteConfig.authors,
-  creator: siteConfig.creator,
-  openGraph: siteConfig.openGraph,
-  twitter: siteConfig.twitter,
-  robots: siteConfig.robots,
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/en',
-      'fa-IR': '/fa',
-      'ar-SA': '/ar'
-    }
-  },
-  manifest: '/manifest.json',
-  icons: {
-    icon: [
-      { url: './image/Logo.svg', type: 'image/svg+xml' },
-      { url: './image/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: './image/favicon-16x16.png', sizes: '16x16', type: 'image/png' }
-    ],
-    apple: [
-      { url: './image/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-    ],
-    other: [
-      { rel: 'mask-icon', url: './image/safari-pinned-tab.svg', color: '#1890ff' }
-    ]
-  }
 };
 
 // root layout
