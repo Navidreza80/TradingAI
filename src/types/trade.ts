9734044261e509ef149c4f0103354a1b0eb7061d
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-02-28 12:47:14 +0330

    Trade page backend

---
+++ src/types/trade.ts
@@ -0,0 +1,34 @@
+export type Coin = {
+  id: string;
+  name: string;
+  symbol: string;
+  current_price: number;
+  image: string;
+};
+
+export interface Position {
+  symbol: string;
+  amount: number;
+  leverage: number;
+  mode: "cross" | "isolated";
+  type: "LONG" | "SHORT";
+  entryPrice: number;
+  takeProfit: number | null;
+  stopLoss: number | null;
+  timestamp: number;
+  margin?: number;
+  maintenanceMargin?: number;
+}
+
+export interface PriceMap {
+  [symbol: string]: number;
+}
+
+export interface ClosedPosition extends Position {
+  closePrice: number;
+  closeTime: number;
+  pnl: {
+    amount: number;
+    percentage: number;
+  };
+}
